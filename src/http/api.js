3610ddf4f9cb29a1d1d56825d82dd61ddd134445
s1607011324 <1607011324@qq.com>
2021-10-09 17:22:13 +0800

    去除无用接口，更新歌单编号

--- src/http/api.js
+++ src/http/api.js
@@ -87,38 +87,38 @@ let getParamsAndKey = (encParams)=>{
  * @param {*} id 
  * @returns 
  */
-export const getParamsAndKey = (id) => {
-    var i3x = {
-        csrf_token: "",
-        encodeType: "aac",
-        ids: "[" + id + "]",
-        level: "standard"
-    }
-    let pa = window.asrsea(JSON.stringify(i3x), bva9R(["流泪", "强"]), bva9R(Tu1x.md), bva9R(["爱心", "女孩", "惊恐", "大笑"]));
-    // console.log(pa)
-    return {
-        'params': pa.encText,
-        'encSecKey': pa.encSecKey
-    }
-}
+// export const getParamsAndKey = (id) => {
+//     var i3x = {
+//         csrf_token: "",
+//         encodeType: "aac",
+//         ids: "[" + id + "]",
+//         level: "standard"
+//     }
+//     let pa = window.asrsea(JSON.stringify(i3x), bva9R(["流泪", "强"]), bva9R(Tu1x.md), bva9R(["爱心", "女孩", "惊恐", "大笑"]));
+//     // console.log(pa)
+//     return {
+//         'params': pa.encText,
+//         'encSecKey': pa.encSecKey
+//     }
+// }
 
 
 
 
 
-export const getMusicLyric = (id) => {
-    return axios({
-        url: "/sync/getParamsAndKey",
-        method: "post",
-        data:qs.stringify(data),
-        config: {
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-            },
-            timeout: 10000
-        }
-    })
-}
+// export const getMusicLyric = (id) => {
+//     return axios({
+//         url: "/sync/getParamsAndKey",
+//         method: "post",
+//         data:qs.stringify(data),
+//         config: {
+//             headers: {
+//                 'content-type': 'application/x-www-form-urlencoded',
+//             },
+//             timeout: 10000
+//         }
+//     })
+// }
 
 // csrf_token: "1b2bd858338b2d6e5c4dfee29c134883"
 // id: "6962426121"
@@ -127,41 +127,41 @@ export const getMusicLyric = (id) => {
 // offset: "0"
 // total: "true"
 
-export const getMusicList = (data) => {
-    let d =  getParamsAndKey(data);
-    return axios({
-        url: "/sync/getMusicList",
-        method: "post",
-        data:qs.stringify(d),
-        config: {
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-            },
-            timeout: 10000
-        }
-    })
-}
+// export const getMusicList = (data) => {
+//     let d =  getParamsAndKey(data);
+//     return axios({
+//         url: "/sync/getMusicList",
+//         method: "post",
+//         data:qs.stringify(d),
+//         config: {
+//             headers: {
+//                 'content-type': 'application/x-www-form-urlencoded',
+//             },
+//             timeout: 10000
+//         }
+//     })
+// }
 
 
 
 
 
 
-//https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token=
-//post
-export const PostByPlayerUrl = (data) => {
-    return axios({
-        url: "/sync/getParamsAndKey",
-        method: "post",
-        data:qs.stringify(data),
-        config: {
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-            },
-            timeout: 10000
-        }
-    })
-}
+// //https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token=
+// //post
+// export const PostByPlayerUrl = (data) => {
+//     return axios({
+//         url: "/sync/getParamsAndKey",
+//         method: "post",
+//         data:qs.stringify(data),
+//         config: {
+//             headers: {
+//                 'content-type': 'application/x-www-form-urlencoded',
+//             },
+//             timeout: 10000
+//         }
+//     })
+// }
 
 
 
