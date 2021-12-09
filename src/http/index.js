92260a333b22ab259f0c00cdb1dc4caf74542832
s1607011324 <1607011324@qq.com>
2021-12-09 21:03:03 +0800

    修复跨域

--- src/http/index.js
+++ src/http/index.js
@@ -2,9 +2,11 @@
 import axios from 'axios'
 import VueCookies from "vue-cookies";
 // console.log(import.meta.env.VITE_ENV_BASE_URL) // 打印 VITE_ENV_BASE_URL 变量
+
 //创建axios的一个实例 
 var instance = axios.create({
     baseURL: import.meta.env.VITE_ENV_BASE_URL, //接口统一域名'http://localhost:9999',
+    // withCredentials: true,// 全局设置 axios 发送请求带上cookie
     timeout: 6000, //设置超时
     headers: {
         // 'Content-Type': 'application/json;charset=UTF-8;',
